38afa950fd320c14955b5de1a8bb231a67d05138
Igor Shah <zombee1992@gmail.com>
2022-04-13 00:00:42 +0300

    dream-team +

--- src/dream-team.js
+++ src/dream-team.js
@@ -14,20 +14,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code her
+function createDreamTeam(team) {
+  if(!team){return false}
+  if(!Array.isArray(team)){return false}
+  return team.map(name => {
+    if (typeof (name) === 'string') {
+      return name.replace(/\s+/, '')[0].toUpperCase()
+    } else {
+      return ''
+    }
+  }).sort().join('')
 }
+
+console.log(createDreamTeam(false))
+console.log(createDreamTeam(null))
+console.log(createDreamTeam(undefined))
+console.log(createDreamTeam({}))
+
 
 module.exports = {
   createDreamTeam
 };
 
 // let team = ['Matt', 'Ann', 'Dmitry', null, [], 'Max'];
-let team = ['Olivia', 1111, 'Lily', 'Oscar', true, null]
-let x = team && team.map(name => {
-    let regexp = /[a-zA-z]/gmi
-    return name && !Array.isArray(name) && regexp.test(name[0]) ? name[0].toUpperCase() : ''
-  }).sort().join('')
 
-console.log(x)
+// let x = team && team.map(name => {
+//   let regexp = /[a-zA-z]/gmi
+//   return  && regexp.test(name[0]) ? name[0].toUpperCase() : ''
+// }).sort().join('')
+
+// console.log(x)
+// team = ['Peter', 'Adam', 'Gary', 'Bobby', 'Philip', 'Andrea', 'Evelyn', 'Evelyn', 'Kimberly', 'Denise']
+// AABDEEGKPP
+
+//ADGJKMNPRSTW
+
+// console.log(name)
+// console.log(name[0].toUpperCase())
+// let y = team.map(name => {
+//   if (typeof (name) === 'string') {
+//     return name.replace(/\s+/, '')[0].toUpperCase()
+//   } else {
+//     return ''
+//   }
+// }).sort().join('')
+
+// console.log(y)
