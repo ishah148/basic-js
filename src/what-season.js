753ab07d676b2ee9009a5e82287c82784fc51e04
Igor Shah <zombee1992@gmail.com>
2022-04-14 16:39:16 +0300

    what season start

--- src/what-season.js
+++ src/what-season.js
@@ -11,11 +11,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  function getMonthStr(month) {
+    const seasons = ["winter", "winter", "spring", "spring", "spring", "summer", "summer", "summer", "autumn", "autumn", "autumn", "winter"];
+    return seasons[month]
+  }
+
+  return getMonthStr(date.getMonth())
   // remove line with error and write your code here
 }
+
+// console.log(getSeason(new Date(2150, 7, 21, 18, 36, 41, 841)))
 
 module.exports = {
   getSeason
 };
+// const seasons = [
+//   winter,
+//   spring,
+//   summer,
+//   autumn,
+// ]
+// function getMonthStr(month) {
+//   const seasons = ["winter", "winter", "spring", "spring", "spring", "summer", "summer", "summer", "autumn", "autumn", "autumn", "winter"];
+//   return seasons[month]
+// }
+
+// let date = new Date(2020, 02, 31)
+// console.log(date.getMonth())
