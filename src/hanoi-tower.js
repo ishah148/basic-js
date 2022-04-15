efe82d6ae9518b3c600ae8601c9abbd1011a3eef
Igor Shah <zombee1992@gmail.com>
2022-04-15 16:52:07 +0300

    trying...

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -18,7 +18,24 @@ function calculateHanoi(/* disksNumber, turnsSpeed */) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
-
+let res = 0;
+function towerOfHanoi(n, from_rod,  to_rod,  aux_rod)
+{
+        if (n == 0)
+        {
+            return;
+        }
+        towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+        console.log(("Move disk " + n + " from rod " + from_rod +
+        " to rod " + to_rod));
+        towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+        res+=1;
+    }
+ 
+    // Driver code
+    var n = 5; // Number of disks
+    towerOfHanoi(n, 'A', 'C', 'B'); // A, B and C are names of rods
 module.exports = {
   calculateHanoi
 };
+console.log('res',res)
