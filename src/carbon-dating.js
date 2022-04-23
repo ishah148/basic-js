070f36f87d97b15bf1c08e3e7392c0471e806a1f
Igor Shah <zombee1992@gmail.com>
2022-04-23 15:09:36 +0300

    carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,33 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+
+function dateSample(...args) {
+  if (args.length == 0)
+    return false
+  if (typeof args[0] !== 'string')
+    return false
+  if (args[0] === '' || args[0] === ' ' || typeof Number(args[0]) !== 'number' || isNaN(args[0]))
+    return false
+  if(args[0] <= 0 || args[0] > 15 || args[0] === 15.1)
+    return false
+
+  let k = 0.693 / HALF_LIFE_PERIOD
+  let res = Math.log(MODERN_ACTIVITY / args[0]) / k;
+  return (Math.ceil(res, 1) === Infinity) ? false : Math.ceil(res, 1);
 }
 
 module.exports = {
   dateSample
 };
+
+
+console.log(dateSample('3'))
+console.log(dateSample('ACTIVITY OVER 9000'))
+console.log(dateSample(' '))
+
+
+console.log(dateSample(' \n\t\r'))
+console.log(dateSample(' '))
+console.log(dateSample(''))
--- src/what-season.js
+++ src/what-season.js
@@ -12,73 +12,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
+  console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',date)
   if (!date) { return 'Unable to determine the time of year!' }
   let res;
   function getMonthStr(month) {
     const seasons = ["winter", "winter", "spring", "spring", "spring", "summer", "summer", "summer", "autumn", "autumn", "autumn", "winter"];
     return seasons[month]
   }
-  try {
-    // res = getMonthStr(date.getMonth());
-    if (date.getUTCDate()) {
+
+  if (Object.prototype.toString.call(date) === "[object Date]") {
+    if (isNaN(date)) {
+      return 'Invalid date!';
+    } else {
       res = getMonthStr(date.getMonth())
     }
-  } catch (e) {
-    res =  'Invalid date!';
+  } else {
+    return 'Invalid date!';
   }
-  // console.log('date =========',date)
+
   return res;
 }
 
-// console.log(getSeason(new Date(2150, 7, 21, 18, 36, 41, 841)))
 
 
-const fakeDate = {
-  toString() {
-      return Date.prototype.toString.call(new Date());
-  },
-  [Symbol.toStringTag]: 'Date'
-};
 
-const deeperFakeDate = {
-  toString() {
-      return Date.prototype.toString.call(new Date());
-  },
-  getMonth() {
-      return Date.prototype.getMonth.call(new Date());
-  },
-  getFullYear() {
-      return Date.prototype.getFullYear.call(new Date(1994, 1, 2, 3, 4, 5));
-  },
-  getDate() {
-      return Date.prototype.getDate.call(new Date(2020, 0, 3, 4, 5, 6));
-  },
-  getHours() {
-      return Date.prototype.getHours.call(new Date(1978, 2, 4, 5, 6, 7));
-  },
-  getMinutes() {
-      return Date.prototype.getMinutes.call(new Date(202, 3, 5, 6, 7, 8));
-  },
-  getSeconds() {
-      return Date.prototype.getSeconds.call(new Date(1, 4, 6, 7, 8, 9));
-  },
-  getMilliseconds() {
-      return Date.prototype.getMilliseconds.call(new Date(2019, 7, 8, 9, 10, 11));
-  },
-  getDay() {
-      return Date.prototype.getDay.call(new Date(1812, 8, 9, 10, 11, 12));
-  },
-  [Symbol.toStringTag]: 'Date'
-};
+
 
 module.exports = {
   getSeason
 };
-
-console.log(getSeason('foo'))
-console.log(getSeason({ John: 'Smith' }))
-console.log(getSeason(20192701))
-console.log(getSeason([2019, '27', 0 + '1']))
-console.log(getSeason(() => new Date()))
-console.log(getSeason(fakeDate))
-console.log(getSeason(deeperFakeDate))
