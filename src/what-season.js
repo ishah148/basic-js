e1ae4b73788eda45a8cb5164ac2fcc965d7bd784
Igor Shah <zombee1992@gmail.com>
2022-05-01 21:12:28 +0300

    123

--- src/what-season.js
+++ src/what-season.js
@@ -12,7 +12,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
-  throw new NotImplementedError('Not implemented');
+  // throw new NotImplementedError('Not implemented');
   console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',date)
   if (!date) { return 'Unable to determine the time of year!' }
   let res;
